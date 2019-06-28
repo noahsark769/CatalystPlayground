12b91e2a77dbc1d8f91ab35836e6d54e672e71df
Noah Gilmore <noah.w.gilmore@gmail.com>
2019-06-27 19:27:30 -0700

    Add touch bar examples

--- UIKitForMacPlayground/ViewController.swift
+++ UIKitForMacPlayground/ViewController.swift
@@ -7,8 +7,23 @@
 //
 
 import UIKit
+import AppKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NSTouchBarProvider {
+    var touchBar: NSTouchBar? {
+        let bar = NSTouchBar()
+        let identifier = NSTouchBarItem.Identifier(rawValue: "clickme")
+        bar.defaultItemIdentifiers = [identifier]
+        bar.templateItems = [
+            NSButtonTouchBarItem(
+                identifier: identifier,
+                title: "Click me",
+                target: self,
+                action: #selector(didTapClickMe(_:))
+            )
+        ]
+        return bar
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +31,24 @@ class ViewController: UIViewController {
         self.view.backgroundColor = .red
     }
 
+    @objc private func didTapClickMe(_ sender: NSTouchBarItem) {
+        print("Click me!!!")
+    }
 
+    override func buildCommands(with builder: UICommandBuilder) {
+        <#code#>
+    }
 }
 
+//extension ViewController: NSTouchBarDelegate {
+//    func touchBar(_ touchBar: NSTouchBar, makeItemForIdentifier identifier: NSTouchBarItem.Identifier) -> NSTouchBarItem? {
+//        if identifier.rawValue == "something" {
+//            return NSButtonTouchBarItem(identifier: identifier, title: "Click me", image: UIImage(systemName: "hurricane")!, target: self, action: #selector(didTapClickMe(_:)))
+//        }
+//        return nil
+//    }
+//
+//    @objc private func didTapClickMe(_ sender: NSTouchBarItem) {
+//        print("Click me!!!")
+//    }
+//}
